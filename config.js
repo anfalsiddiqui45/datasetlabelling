fda794d1f3461b218a5be0ccd43b5aa2a6230c76
Anfal <anfal@gmail.com>
2026-07-23 16:19:13 -0400

    Enable remote access: commit config.js for GitHub Pages, keep serviceAccountKey.json private

---
+++ config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+window.firebaseConfig = {
+  apiKey: "AIzaSyBGu78CjMtUyxEZV7nzas6X0uzsjSCAdN0",
+  authDomain: "datasetlabelling-f6dc3.firebaseapp.com",
+  projectId: "datasetlabelling-f6dc3",
+  storageBucket: "datasetlabelling-f6dc3.firebasestorage.app",
+  messagingSenderId: "199079948358",
+  appId: "1:199079948358:web:a2af77f8ad2f3a6af45a47",
+  measurementId: "G-JSR3D9RCJV"
+};
